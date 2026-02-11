0b66967e0cf02a81cfc3d506345c6b83f3f33c16
lee-gi-tae <sagamo857@naver.com>
2026-02-11 16:58:23 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.00 ms, Memory: 9.21 MB -BaekjoonHub

---
+++ 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.py
@@ -0,0 +1,9 @@
+def solution(price):
+    if price >= 500000:
+        price *= 0.8
+    elif 300000 <= price < 500000:
+        price *= 0.9
+    elif 100000 <= price < 300000:
+        price *= 0.95
+    return int(price)
+    
